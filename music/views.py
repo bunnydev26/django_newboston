de28d1fe8197cc2ce86123b272de262f42f14564
Amit Thapa <amit@okay.com>
2017-09-06 23:43:46 +0530

    First View & template

--- music/views.py
+++ music/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Album
+
 
 # Create your views here.
+def index(request):
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = { 'all_albums': all_albums, }
+
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
