146e49a1b91058eee2ba8753fdcfb65db5a9a057
Amit Thapa <amit@okay.com>
2017-09-07 22:16:52 +0530

    details page

--- music/models.py
+++ music/models.py
@@ -14,3 +14,6 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     song_title = models.CharField(max_length=250)
     file_type = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Album
 
@@ -7,8 +7,17 @@ from .models import Album
 # Create your views here.
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
+    # template = loader.get_template('music/index.html')
     context = { 'all_albums': all_albums, }
-
     # return HttpResponse(template.render(context, request))
     return render(request, 'music/index.html', context)
+
+def detail(request, album_id):
+    # try:
+    #     album = Album.objects.get(id=album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404("Album doesnot exists")
+
+    album = get_object_or_404(Album, id=album_id)
+
+    return render(request, 'music/detail.html', {'album' : album})
