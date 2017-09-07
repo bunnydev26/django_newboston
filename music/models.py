6fe8e5f17d0ae130cda0edcb08e1d806b5f71414
Amit Thapa <amit@okay.com>
2017-09-08 00:27:08 +0530

    First Form and handled POST REQUEST

--- music/models.py
+++ music/models.py
@@ -14,6 +14,7 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     song_title = models.CharField(max_length=250)
     file_type = models.CharField(max_length=10)
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.song_title
--- music/views.py
+++ music/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
-from .models import Album
+from .models import Album, Song
 
 
 # Create your views here.
@@ -21,3 +21,17 @@ def detail(request, album_id):
     album = get_object_or_404(Album, id=album_id)
 
     return render(request, 'music/detail.html', {'album' : album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    song_id = request.POST['song']
+    error_message = None
+    try:
+        song = album.song_set.get(pk=song_id)
+    except (KeyError, Song.DoesNotExist):
+        error_message = 'You didnot select a valid song'
+    else:
+        song.is_favorite = True
+        song.save()
+
+    return render(request, 'music/detail.html', {'album': album, 'error_message':error_message})
